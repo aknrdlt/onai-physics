a739d9f8d18c2bab2eacefd2cebf0ac4fa6c7a21
Aknurdaulet.Sarkytbayev <49584b3e3870a8523588738ec4cdd883efcc31db>
2021-05-22 10:06:11 +0600

    collect staticfiles

--- OnaiPhysics/settings.py
+++ OnaiPhysics/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -131,10 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "../static"),
+    os.path.join(BASE_DIR, "./static"),
 )
 
 MEDIA_URL = '/images/'
